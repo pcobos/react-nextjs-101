6835889e77a0572a75337a995a3ac0737de71ead
Pedro Cobos <pedro.cobos@live.com>
2021-03-11 21:40:09 -0600

    feat: added styles to footer and navbar components

--- components/navbar.js
+++ components/navbar.js
@@ -2,14 +2,14 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return ( 
-    <div>
-      <div>
+    <nav>
+      <div className="logo">
         <h1>Logo</h1>
       </div>
         <Link href="/"><a>Home</a></Link>
         <Link href="/about"><a>About</a></Link>
         <Link href="/ninjas"><a>All Ninjas</a></Link>
-    </div>
+    </nav>
    );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <h1>Test</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem vitae voluptate at, possimus beatae perferendis asperiores eum quisquam ipsum et numquam. Fuga, est eos. Enim exercitationem perferendis facilis ad soluta.</p>
-      <Footer />
     </div>
   )
 }
